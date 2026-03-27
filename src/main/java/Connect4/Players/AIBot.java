1c05b71d7065a7c5f20a9df2d16dffbf8034d60b
rodrigonicastro <rofr4171@colorado.edu>
2026-03-27 13:38:32 -0600

    started adding determineMove logic for AI player

--- src/main/java/Connect4/Players/AIBot.java
+++ src/main/java/Connect4/Players/AIBot.java
@@ -27,11 +27,56 @@ public class AIBot extends Player{
 
     private int getDefenseRating(){
         //analyze board to determine how effective a defense would be (how many coins P1 has in a row)
+        Board gameBoard = Board.getInstance();
+        int biggestRating = 0;
+
+        for(int i = 0; i < gameBoard.getNumColumns(); i ++){
+            int verticalRating = getVerticalRatingForColumn(i, getPlayer());
+            int horizontalRating = getHorizontalRatingForColumn(i, getPlayer());
+            int diagonalRating = getDiagonalRatingForColumn(i, getPlayer());
+
+            biggestRating = Math.max(biggestRating,
+                            Math.max(verticalRating,
+                            Math.max(horizontalRating,
+                                     diagonalRating)));
+        }
         return 0;
     }
 
     private int getAttackRating(){
         //analyze board to determine how effective an attack would be(how many coins P2 has in a row)
         return 0;
+    }
+
+    private int getVerticalRatingForColumn(int column, int player){
+        Board gameBoard = Board.getInstance();
+        int rating = 0;
+
+        //find in which row the coin would land in this column
+        int landingRow = -1;
+        for(int i = 0; i < gameBoard.getNumRows(); i++){
+            if(gameBoard.getBoard()[i][column] == 0){
+                landingRow = i;
+                break;
+            }
+        }
+
+        if(landingRow <= 0) return rating; //-1 means column is full, 0 means column is empty. On either case, rating is 0
+
+        //look down starting from where the coin would land in this column and count how many of this player's coins are there
+        for(int i = landingRow-1; i >= 0; i--){
+            if(gameBoard.getBoard()[landingRow][column] == player) rating++;
+            else break;
+        }
+
+        return rating;
+    }
+
+    private int getHorizontalRatingForColumn(int column, int player){
+
+    }
+
+    private int getDiagonalRatingForColumn(int column, int player){
+
     }
 }
